f16d007ab8b91ad688510b3651fb72f5b38f0e2f
Odaguchi_Yuta <c221012@ivy.ac.jp>
2024-02-16 13:31:17 +0900

    UIの追加

---
+++ yummy_fight/Assets/Script/GameManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] GameObject cardPrefab;
+    [SerializeField] Transform playerHand;
+
+    void Start()
+    {
+        Draw();
+    }
+
+    public void Draw()
+    {
+        // èD‚ğ1–‡”z‚éi©•ªj
+        Instantiate(cardPrefab, playerHand);
+    }
+}
