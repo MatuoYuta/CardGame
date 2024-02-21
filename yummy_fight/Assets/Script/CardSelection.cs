d0320fc80e73e5b1519a8df2cc30e4b8d88fedbe
ilasutoriasu <vaioretoriri@gmail.com>
2024-02-21 11:56:16 +0900

    攻撃ボタンの表示、非表示の処理（模索中）

--- yummy_fight/Assets/Script/CardSelection.cs
+++ yummy_fight/Assets/Script/CardSelection.cs
@@ -3,13 +3,38 @@ using UnityEngine.UI;
 
 public class CardSelection : MonoBehaviour
 {
-    // 攻撃ボタン
-    public Button attackButton;
+    public GameObject attackButton;
 
-    // カードが選択されたときに呼ばれる関数
-    public void OnCardSelected()
+    public bool cardSelected = false;
+
+    void Start()
     {
-        // カードが選択された場合、攻撃ボタンを表示する
-        attackButton.gameObject.SetActive(true);
+        // 最初は攻撃ボタンを非表示にする
+        attackButton.gameObject.SetActive(false);
+    }
+
+    void Update()
+    {
+        // マウスの左クリックが押されたかどうかを確認し、カードがクリックされた場合にフラグを設定する
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Vector2 ray2D = new Vector2(ray.origin.x, ray.origin.y);
+            RaycastHit2D hit2D = Physics2D.Raycast(ray2D, Vector2.zero);
+            if (hit2D.collider != null && hit2D.collider.gameObject.CompareTag("Card"))
+            {
+                cardSelected = (true);
+            }
+        }
+
+        // カードが選択されているかどうかを確認し、攻撃ボタンの表示状態を更新する
+        if (cardSelected)
+        {
+            attackButton.gameObject.SetActive(true);
+        }
+        else
+        {
+            attackButton.gameObject.SetActive(false);
+        }
     }
 }
