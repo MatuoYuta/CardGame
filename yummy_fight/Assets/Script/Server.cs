63c4b12fd2a4a8c5b8ab6aeb886ca915a83e2927
tanakaakito <aki.tana.1114@gmail.com>
2024-02-21 11:46:56 +0900

    サーバー処理変更

--- yummy_fight/Assets/Script/Server.cs
+++ yummy_fight/Assets/Script/Server.cs
@@ -3,13 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 using Photon.Realtime;
+using TMPro;
 
 public class Server : MonoBehaviourPunCallbacks
 {
+    public GameObject LoginPanel;
+    public TMP_InputField playerNameInput;
+
+    public GameObject ConnectingPanel;
+    public GameObject LobbyPanel;
+
     // Start is called before the first frame update
     void Start()
     {
-        PhotonNetwork.ConnectUsingSettings();
+        PhotonNetwork.AutomaticallySyncScene = true;
+        LoginPanel.SetActive(true);
+        ConnectingPanel.SetActive(false);
+        LobbyPanel.SetActive(false);
     }
 
     // Update is called once per frame
@@ -17,8 +27,52 @@ public class Server : MonoBehaviourPunCallbacks
     {
         
     }
+
+    public void ConnectToPhotonServer() //LoginButtonで呼ぶ
+    {
+        if (!PhotonNetwork.IsConnected) //サーバーに接続していたら
+        {
+            string playerName = playerNameInput.text;
+            if (!string.IsNullOrEmpty(playerName))
+            {
+                PhotonNetwork.LocalPlayer.NickName = playerName;
+                PhotonNetwork.ConnectUsingSettings();
+                ConnectingPanel.SetActive(true);
+                LoginPanel.SetActive(false);
+            }
+        }
+        else { }
+    }
+
+    public void JoinRandomRoom() //StartButtonで呼ぶ
+    {
+        PhotonNetwork.JoinRandomRoom();
+    }
     public override void OnConnectedToMaster()
     {
-        base.OnConnectedToMaster();
+        LobbyPanel.SetActive(true);
+        ConnectingPanel.SetActive(false);
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        CreateAndJoinRoom(); //ルームがなければ自ら作って入る
+    }
+
+    public override void OnJoinedRoom() //ルームに入ったら呼ばれる
+    {
+        //Debug.Log(PhotonNetwork.NickName+ "joined to"+ PhotonNetwork.CurrentRoom.Name);
+        PhotonNetwork.LoadLevel("GameScene"); //シーンをロード
+    }
+
+    void CreateAndJoinRoom()
+    {
+        //自動で作られるルームの設定
+        string roomName = "Room" + Random.Range(0, 10000); //ルーム名
+        RoomOptions roomOptions = new RoomOptions();
+        roomOptions.IsOpen = true;
+        roomOptions.IsVisible = true;
+        roomOptions.MaxPlayers = 2;　//ルームの定員
+        PhotonNetwork.CreateRoom(roomName, roomOptions);
     }
 }
