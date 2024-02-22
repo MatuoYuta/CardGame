db30fa99faa7f2d6ec6b45e01585c0e01c39af34
ilasutoriasu <vaioretoriri@gmail.com>
2024-02-22 15:46:41 +0900

    スクリプトの修正と削除

--- yummy_fight/Assets/Script/CardSelection.cs
+++ yummy_fight/Assets/Script/CardSelection.cs
@@ -4,13 +4,12 @@ using UnityEngine.UI;
 public class CardSelection : MonoBehaviour
 {
     public GameObject attackButton;
-
     public bool cardSelected = false;
 
     void Start()
     {
         // 最初は攻撃ボタンを非表示にする
-        attackButton.gameObject.SetActive(false);
+        attackButton.SetActive(false);
     }
 
     void Update()
@@ -18,23 +17,37 @@ public class CardSelection : MonoBehaviour
         // マウスの左クリックが押されたかどうかを確認し、カードがクリックされた場合にフラグを設定する
         if (Input.GetMouseButtonDown(0))
         {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            Vector2 ray2D = new Vector2(ray.origin.x, ray.origin.y);
-            RaycastHit2D hit2D = Physics2D.Raycast(ray2D, Vector2.zero);
-            if (hit2D.collider != null && hit2D.collider.gameObject.CompareTag("Card"))
+            Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            // Raycastの可視化用に、Rayの始点と終点を計算
+            Vector2 origin = transform.position; // カードの位置を始点とする
+            Vector2 direction = mousePosition - origin; // カードからマウス位置への方向を終点とする
+
+            // Raycastを実行
+            RaycastHit2D[] hits = Physics2D.RaycastAll(origin, direction);
+
+            foreach (RaycastHit2D hit in hits)
             {
-                cardSelected = (true);
+                if (hit.collider != null && hit.collider.gameObject.CompareTag("Card"))
+                {
+                    cardSelected = true;
+                    Debug.Log("Card Clicked: " + hit.collider.gameObject.name); // デバッグログでクリックされたカードの名前を表示
+                    OnCardHit(); // カードがクリックされた時の追加の処理を実行
+                    break;
+                }
             }
+
+            // Rayの可視化
+            Debug.DrawLine(origin, origin + direction, Color.red, 0.1f);
         }
 
         // カードが選択されているかどうかを確認し、攻撃ボタンの表示状態を更新する
-        if (cardSelected)
-        {
-            attackButton.gameObject.SetActive(true);
-        }
-        else
-        {
-            attackButton.gameObject.SetActive(false);
-        }
+        attackButton.SetActive(cardSelected);
+    }
+
+    // カードがクリックされた時の処理
+    void OnCardHit()
+    {
+        // カードがクリックされた時の追加の処理をここに記述する
     }
 }
