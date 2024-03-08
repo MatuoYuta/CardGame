e7ebac7e84e1871ae051c569bf361ca115c0ca56
ilasutoriasu <vaioretoriri@gmail.com>
2024-03-08 11:40:25 +0900

    awsedr

--- yummy_fight/Assets/Script/AttackButton.cs
+++ yummy_fight/Assets/Script/AttackButton.cs
@@ -6,11 +6,20 @@ using UnityEngine.UI;
 public class AttackButton : MonoBehaviour
 {
     public GameObject cardObject; // 横向きにするCardオブジェクト
+    private Button attackButton; // ボタンの参照
+
+    void Start()
+    {
+        // ボタンコンポーネントを取得
+        attackButton = GetComponent<Button>();
+    }
 
     public void OnAttackButtonClick()
     {
         Debug.Log("相手に攻撃した");
         RotateCard(); // カードを横向きにするメソッドを呼び出す
+        // ボタンを非表示にする
+        gameObject.SetActive(false);
     }
 
     void RotateCard()
