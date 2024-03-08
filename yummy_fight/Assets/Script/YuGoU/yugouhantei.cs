61caba0203912ad6e853c06e27a2df6a0bbc6ec1
matuo2525 <yu-.55ta@docomo.ne.jp>
2024-03-08 13:53:11 +0900

    えｄｒｆｇ

--- yummy_fight/Assets/Script/YuGoU/yugouhantei.cs
+++ yummy_fight/Assets/Script/YuGoU/yugouhantei.cs
@@ -11,6 +11,9 @@ public class yugouhantei : MonoBehaviour
     public GameObject popup;
     public Transform playerField;
 
+    public int yugou = 0;
+    
+
     public Bagamu bagam;
     // Start is called before the first frame update
     void Start()
@@ -18,13 +21,84 @@ public class yugouhantei : MonoBehaviour
         manage_script = GameObject.Find("GameManager").GetComponent<GameManager>();
         directer = GameObject.Find("GameDirecter").GetComponent<GameDirecter>();
         bagam = GameObject.Find("bagamute").GetComponent<Bagamu>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-       
+        //Yugousyoukan();
+        OnClick();
     }
 
-  
-}
+    public void Yugousyoukan()
+    {
+        switch (yugou)
+        {
+            case 101:
+                break;
+            case 102:
+                break;
+            case 103:
+                break;
+            case 104:
+                break;
+            case 105:
+                manage_script.CreateCard(105, playerField);
+                for (int i = 0; i < directer.playerkitchenCardList.Length; i++)
+                {
+                    if (directer.playerkitchenCardList[i].gameObject.GetComponent<CardView>().cardID == 2)
+                    {
+                        Destroy(directer.playerkitchenCardList[i].gameObject);
+                        Debug.Log("ëfçﬁçÌèú");
+                        for (int a = 0; a < directer.playerkitchenCardList.Length; a++)
+                        {
+                            if (directer.playerkitchenCardList[a].gameObject.GetComponent<CardView>().cardID == 1 || directer.playerkitchenCardList[a].gameObject.GetComponent<CardView>().cardID == 3)
+                            {
+                                Destroy(directer.playerkitchenCardList[a].gameObject);
+                                Debug.Log("ëfçﬁçÌèú2");
+                                popup.SetActive(false);
+                            }
+                        }
+                    }
+                }
+                break;
+
+        }
+    }
+    public void OnClick()
+    {
+        switch (yugou)
+        {
+            case 101:
+                break;
+            case 102:
+                break;
+            case 103:
+                break;
+            case 104:
+                break;
+            case 105:
+                manage_script.CreateCard(105, playerField);
+                for (int i = 0; i < directer.playerkitchenCardList.Length; i++)
+                {
+                    if (directer.playerkitchenCardList[i].gameObject.GetComponent<CardView>().cardID == 2)
+                    {
+                        Destroy(directer.playerkitchenCardList[i].gameObject);
+                        Debug.Log("ëfçﬁçÌèú");
+                        for (int a = 0; a < directer.playerkitchenCardList.Length; a++)
+                        {
+                            if (directer.playerkitchenCardList[a].gameObject.GetComponent<CardView>().cardID == 1 || directer.playerkitchenCardList[a].gameObject.GetComponent<CardView>().cardID == 3)
+                            {
+                                Destroy(directer.playerkitchenCardList[a].gameObject);
+                                Debug.Log("ëfçﬁçÌèú2");
+                                popup.SetActive(false);
+                            }
+                        }
+                    }
+                }
+                break;
+
+        }
+    }
+    }
