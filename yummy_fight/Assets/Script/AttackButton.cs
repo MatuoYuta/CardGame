c32be8b62674c7f5d5063d421ec2749164513286
ilasutoriasu <vaioretoriri@gmail.com>
2024-03-12 09:51:19 +0900

    aq

--- yummy_fight/Assets/Script/AttackButton.cs
+++ yummy_fight/Assets/Script/AttackButton.cs
@@ -6,7 +6,6 @@ using UnityEngine.UI;
 public class AttackButton : MonoBehaviour
 {
     public GameObject cardObject; // 横向きにするCardオブジェクト
-    public int enemylife = 5; // Enemy's life
     private Button attackButton; // ボタンの参照
 
     void Start()
@@ -19,17 +18,36 @@ public class AttackButton : MonoBehaviour
     {
         Debug.Log("相手に攻撃した");
         RotateCard(); // カードを横向きにするメソッドを呼び出す
+        AttackPlayer(); // プレイヤーに攻撃するメソッドを呼び出す
         // ボタンを非表示にする
         gameObject.SetActive(false);
-
-        // Decrease enemy's life
-        enemylife--;
-        Debug.Log("Enemy's life: " + enemylife);
     }
 
     void RotateCard()
     {
         // カードを90度回転させる
         cardObject.transform.Rotate(new Vector3(0f, 0f, 90f));
+    }
+
+    void AttackPlayer()
+    {
+        // プレイヤーオブジェクトをタグで検索して取得
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+
+        // プレイヤーオブジェクトが存在する場合
+        if (player != null)
+        {
+            // プレイヤーオブジェクトにダメージを与えるなどの処理をここに記述
+            // 以下はダメージを与える処理の例
+            PlayerHealth playerHealth = player.GetComponent<PlayerHealth>();
+            if (playerHealth != null)
+            {
+                playerHealth.TakeDamage(1); // 例として10のダメージを与える
+            }
+        }
+        else
+        {
+            Debug.LogWarning("プレイヤーオブジェクトが見つかりませんでした！");
+        }
     }
 }
